95744c1bc02d7b19f9b7796c526f9f0616022cf1
Vamshi <vam.1207@gmail.com>
2025-09-22 01:04:42 +0530

    Added MavenJava files

---
+++ src/main/java/SE/JenJava/App.java
@@ -0,0 +1,10 @@
+package SE.JenJava;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hai, Welcome to the JavaJen application.");
+    }
+}
